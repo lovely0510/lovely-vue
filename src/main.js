645833dbbc6c6be0f73cefe95024937b77d59712
Rodelompad <ompadrodel03@gmail.com>
2024-04-01 20:38:57 +0800

    Updates

--- src/main.js
+++
@@ -1,9 +0,0 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import './assets/bootstrap.css'
-import './assets/bootstrap.min.css'
-import './assets/bootstrap-grid.css'
-import './assets/bootstrap-grid.min.css'
-import './assets/style.css'
-
-createApp(App).mount('#app')
